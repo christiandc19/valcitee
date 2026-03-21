ac23d8c2f00bdcd41297951f27cbde87941d71aa
github_christiandc19 <christiandc19@hotmail.com>
2026-03-21 10:46:00 -0700

    Fix router basename for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-import { router } from "./router";
+import { createBrowserRouter } from "react-router-dom";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+    },
+  ],
+  {
+    basename: "/valcitee",
+  }
 );
